9572548a0f8356c6398a7107e713b18d3d9e2c7f
Sam Felton <sf.samfelton@icloud.com>
2026-07-26 15:31:16 +0100

    add stack indicator and hearts

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,20 @@
 import { defineConfig, fontProviders } from "astro/config";
+import icon from "astro-icon";
 
 export default defineConfig({
+  integrations: [icon()],
+
   fonts: [
     {
       provider: fontProviders.fontsource(),
       name: "Unbounded",
       cssVariable: "--font-unbounded",
+    },
+    {
+      provider: fontProviders.fontsource(),
+      name: "Overpass",
+      cssVariable: "--font-overpass",
+      weights: [400, 500, 700],
     },
   ],
 });
